6280b8854250ef9423f8f3cfaeb0c44a0d25a6fa
Tettey1 <tetteynathan89@gmail.com>
2024-04-19 10:20:18 +0000

    modified:   src/App.js 	new file:   src/features/notes/TimeAgo.js 	modified:   src/features/notes/notesSlice.js 	modified:   src/style.css

--- src/App.js
+++ src/App.js
@@ -1,94 +1,8 @@
-// import { useSelector } from 'react-redux';
-// import { useDispatch } from 'react-redux';
-// import { addNote,editNote,deleteNote } from './features/notes/notesSlice';
-// import { nanoid } from '@reduxjs/toolkit';
-// import { useState } from 'react';
 
-// const App = () => {
-//   const dispatch = useDispatch();
-
-//   const [editMode, setEditMode] = useState(false);
-//   const [editNoteId, setEditNoteId] = useState(null);
-  
-//   const [title, setTitle] = useState('');
-//   const [content, setContent] = useState('');
-
-//   const notes = useSelector((state) => state.notes);
-
-
-//   const handleAddNote = () => {
-//     if (title.trim() !== '' && content.trim() !== '') {
-//       if (editMode) {
-//         dispatch(editNote({ id: editNoteId, title, content }));
-//         setEditMode(false);
-//         setEditNoteId(null);
-//       } else {
-//         dispatch(addNote({ id: nanoid(), title, content }));
-//       }
-//       setTitle('');
-//       setContent('');
-//     }
-//   };
-
-//   const handleEditNote = (note) => {
-//     setTitle(note.title);
-//     setContent(note.content);
-//     setEditMode(true);
-//     setEditNoteId(note.id);
-//   };
-  
-//   const handleDeleteNote = (id) => {
-//     dispatch(deleteNote({ id }));
-//   };
-
-
-//   return (
-//     <>
-//     <section className="w-1/2 m-auto">
-//     <h1 className='text-center'>Notes App</h1>
-//       {/* Render form for adding/editing note */}
-//       <form onSubmit={handleAddNote}>
-//       <input
-//         type="text"
-//         className='text-black'
-//         placeholder='Title'
-//         value={title}
-//         onChange={(e) => setTitle(e.target.value)}
-//       />
-//       <textarea
-//         className='text-black'
-//         placeholder='Enter a new Note Here...'
-//         value={content}
-//         onChange={(e) => setContent(e.target.value)}
-//       />
-//       <button className="bg-blue-500 rounded-md px-3 py-1 text-white border border-blue-600 hover:bg-blue-600" type="submit"
-//       onClick={handleAddNote}
-//       >{editMode ? 'Update Note' : 'Add Note'}</button>
-//     </form>
-//     </section>  
-//       <ul>
-//       {notes.map((note) => (
-//       <li className="grid lg:grid-cols-3" key={note.id}>
-//         <h2>{note.title}</h2>
-//         <p>{note.content}</p>
-        
-//         <button className="bg-green-500 text-sm rounded-md px-3 py-1 text-white border border-green-600 hover:bg-green-600"
-//         onClick={() => handleEditNote(note.id)}>Edit</button>
-//         <button className="bg-red-500 text-sm rounded-md px-3 py-1 text-white border border-red-600 hover:bg-red-600"
-//          onClick={() => handleDeleteNote(note.id)}>Delete</button>
-        
-//       </li>
-//     ))}
-//       </ul>
-  
-//     </>
-//   );
-// };
-
-// export default App;
 import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { addNote, editNote, deleteNote } from './features/notes/notesSlice';
+import TimeAgo from './features/notes/TimeAgo';
 
 function App() {
   const [title, setTitle] = useState('');
@@ -126,13 +40,14 @@ function App() {
   };
 
   return (
-    <div className="w-1/2 m-auto">
-      <h1>Notes App</h1>
+    <>
+    <main className="w-1/2 m-auto border border-white p-6 rounded-lg shadow-md">
+      <h1 className='text-center text-4xl font-bold mb-4'>Notes App</h1>
 
-      <form className='flex justify-center' onSubmit={handleAddNote}>
+      <form onSubmit={handleAddNote} className="space-y-4">
         <input
           type="text"
-          className='text-black'
+          className='text-black block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500'
           placeholder='Title'
           value={title}
           onChange={(e) => setTitle(e.target.value)}
@@ -140,30 +55,43 @@ function App() {
         <textarea
           value={content}
           onChange={(e) => setContent(e.target.value)}
-          placeholder="Content"
-          className='text-black'
+          placeholder="Enter your Notes Here..."
+          className='text-black block w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:border-blue-500'
+          rows='4'
         />
         <div className='flex justify-center'>
-        <button className='w-1/2  bg-blue-500 rounded-md px-3 py-1 text-white border border-blue-600 hover:bg-blue-600"' onClick={handleAddNote}>
+        <button className='w-1/2 bg-blue-500 rounded-md px-3 py-1 text-white border border-blue-600 hover:bg-blue-600'
+         onClick={handleAddNote}>
           {editMode ? 'Save Changes' : 'Add Note'}
         </button>
         </div>
       </form>
-
-        <ul className='grid grid-col-3' >
+      </main>
+        <section className='grid grid-cols-3' >
           {notes.map((note) => (
-  
-            <li className='bg-white border border-white ' key={note.id}>
-              <h3>{note.title}</h3>
-              <p>{note.content}</p>
-              <button onClick={() => handleEditNote(note)} className='edit'>Edit</button>
-              <button onClick={() => handleDeleteNote(note.id)} className='delete'>
+
+            <article className='relative' key={note.id}>
+              <h3 className='relative inline-block'>{note.title}
+              <span class="absolute bottom-0 left-0 w-full h-0.5 bg-blue-500"></span>
+              </h3>
+              <p  className='mb-5'>{note.content.substring(0, 100)}</p>
+
+              <div className='space-x-3 absolute bottom-0 left-2 '>
+              <button onClick={() => handleEditNote(note)} 
+              className="bg-green-500 rounded-md text-sm px-3 py-1 text-white border border-green-600 hover:bg-green-600">Edit</button>
+              <button className="bg-red-500 text-sm rounded-md px-3 py-1 text-white border border-red-600 hover:bg-red-600"
+               onClick={() => handleDeleteNote(note.id)} >
                 Delete
               </button>
-            </li>
+              </div>
+              <span className="absolute font-light text-sm italic bottom-0 right-2">
+              <TimeAgo timestamp={note.date} />
+            </span>
+            </article>
           ))}
-        </ul>
-      </div>
+        </section>
+    </>
+      
   );
 }
 
--- src/features/notes/notesSlice.js
+++ src/features/notes/notesSlice.js
@@ -1,8 +1,32 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { sub } from 'date-fns';
+import { nanoid } from '@reduxjs/toolkit';
+
+
+const initialState = [
+  {
+    id: nanoid(),
+    title: 'Learning Redux Toolkit',
+    content: "I've heard good things.",
+    date: sub(new Date(), { minutes: 20 }).toISOString(),
+  },
+  {
+    id: nanoid(),
+    title: 'Slices...',
+    content: "The more I say slice, the more I want pizza.",
+    date: sub(new Date(), { minutes: 15 }).toISOString(),
+  },
+  {
+    id: nanoid(),
+    title: 'Toolkit...',
+    content: "They say it's makes redux easier...i still don't see how.",
+    date: sub(new Date(), { minutes: 10 }).toISOString(),
+  }
+];
 
 const notesSlice = createSlice({
   name: 'notes',
-  initialState: [],
+  initialState,
   reducers: {
     addNote: (state, action) => {
       state.push(action.payload);
@@ -13,6 +37,7 @@ const notesSlice = createSlice({
       if (existingNote) {
         existingNote.title = title;
         existingNote.content = content;
+        existingNote.date = new Date().toISOString();
       }
     },
     deleteNote: (state, action) => {
